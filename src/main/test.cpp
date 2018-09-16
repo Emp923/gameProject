e489235746c1873de4945bf4dd5d36a608b22f1b
Eric <emp923@gmail.com>
2018-09-16 00:18:00 -0400

    test file

--- src/main/test.cpp
+++ src/main/test.cpp
@@ -1,10 +1,17 @@
 // placeholder source code for project structure
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+
 	cout << "Hello Neighbor!\n";
+	string name = "";
+	cout << "What is your name? ";
+	cin >> name;
+	cout << "Hello, " << name << endl;
 	return 0;
+	
 }
 
